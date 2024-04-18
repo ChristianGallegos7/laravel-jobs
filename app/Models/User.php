36630aa20346ffe710638384c9317c07aff292ec
Christian <christiantenz7@gmail.com>
2024-04-18 12:46:55 -0500

    campo rol added

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+// En el modelo user usamos el MustVerifyEmail para pedir que el email esta verificado para poder usar su cuenta (implements MustVerifyEmail)
+class User extends Authenticatable 
 {
     use HasFactory, Notifiable;
 
@@ -20,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'rol'
     ];
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,22 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+          <!-- Rol-->
+        <div class="mt-4">
+            <x-input-label for="rol" :value="__('¿Que tipo de cuenta tendra en DevJobs?')" />
+            <select 
+                name="rol" 
+                id="rol" 
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+            >
+            <option value="">-- Selecciona un rol --</option>
+            <option value="1">Developer - Obtener Empleo</option>
+            <option value="2">Recruiter - Publicar Empleo</option>
+
+            </select>
+            <x-input-error :messages="$errors->get('rol')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
