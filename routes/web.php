bbe0a57c0038a7c5eb4772dea7a4c76866f6b683
Christian <christiantenz7@gmail.com>
2024-04-18 22:00:50 -0500

    Livewire added

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [VacanteController::class, 'index'])->middleware(['auth', 'verified'])->name('vacantes.index');
+Route::get('/vacantes/create', [VacanteController::class, 'create'])->middleware(['auth', 'verified'])->name('vacantes.create');
+
 
 
 
