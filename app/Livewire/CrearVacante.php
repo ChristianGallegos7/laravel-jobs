5c0bc39c73ff5529d1733edaf0d44cf6a895446a
Christian <Christian@gmail.com>
2024-04-22 09:12:17 +0700

    Ya aplique livewire en el formulario hice las conexiones con wire:model y las validaciones se muestran en el formulario

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -2,12 +2,46 @@
 
 namespace App\Livewire;
 
+use App\Models\Categoria;
+use App\Models\Salario;
 use Livewire\Component;
 
 class CrearVacante extends Component
 {
+    
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required',
+
+    ];
+
+    public function crearVacante(){
+        $datos = $this->validate();
+    }
+
     public function render()
     {
-        return view('livewire.crear-vacante');
+        //Consular Base de Datos para traernos todos los salarios
+        $salarios = Salario::all();
+        $categorias = Categoria::all();
+        
+        return view('livewire.crear-vacante', [
+            'salarios' => $salarios,
+            'categorias' => $categorias,
+        ]);
     }
 }
